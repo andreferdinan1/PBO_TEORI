ddf6374c002bbdf4f0115b2deb210d8f2af5cbdd
andreferdinan1 <andreferdinand276@gmail.com>
2023-04-01 19:43:31 +0700

    first commit

---
+++ Latihan1.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package andreferdinan090323;
+
+/**
+ *
+ * @author Andre Ferdinan
+ * 
+ */
+//modul 4
+public class Latihan1 {
+    public static void main(String[] args){
+        int number =10;//int adalah tipe data, number adalah variable 10=initial value
+        char letter ='a';//char adalah tipe data, letter= variable dan a adalah initial value
+        boolean result = true;//boolean adalah tipe data ,result adalah variable, dan true adalah initial value
+        String str =  "";//string adalah tipe data, str adalah variable
+        
+        str="hello";//hello adalah initial value
+        
+        
+        System.out.println("number = "+number);//print number=10
+        System.out.println("letter = "+letter);//print letter=a
+        System.out.println("result = "+result);//print result=true
+        System.out.println("str= "+str);//print str=hello
+        
+    } 
+}
